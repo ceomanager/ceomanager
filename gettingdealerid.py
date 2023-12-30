745dc49954290cb6b47a122746a370c35025ea02
ceomanager <154714828+ceomanager@users.noreply.github.com>
2023-12-30 11:37:38 +0530

    Add files via upload

---
+++ gettingdealerid.py
@@ -0,0 +1,21 @@
+phone=input("Phone 1 : ")
+phone2=input("Phone 2 : ")
+name=input("Name : ")
+p1=phone[:5]
+p2=phone[5:]
+p3=phone2[:5]
+p4=phone2[5:]
+naam=name[:1]
+he=input('''Enter
+f-food
+m-music
+d-decoration
+l-light
+c-car
+b-lodge
+p-photograpgy
+r-card
+''')
+id="ID = 58"+p1+p4+p3+p2+naam+he
+
+print(id)
